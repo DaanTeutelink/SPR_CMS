5fa93a89af7c443d0f7bd151ae0ca596d9b36e55
Daan <dteutelink@gmail.com>
2025-07-10 14:27:41 +0200

    Hopefully fixed it for vercel

--- client/src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://jwcaojhpbituvbblmomb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3Y2FvamhwYml0dXZiYmxtb21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNDIxMDQsImV4cCI6MjA2NzcxODEwNH0.e1N9xQnH9fqu0a1e6_JkM5ncls4IMXRbG7rhAs3nMRM';
-export const supabase = createClient(supabaseUrl, supabaseKey);
